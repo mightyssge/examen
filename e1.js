dad2666d5baad904a7f90fadc7516188f67a975c
mightyssge <118016003+mightyssge@users.noreply.github.com>
2024-01-19 11:13:46 -0500

    SOPORTE

--- e1.js
+++ e1.js
@@ -7,10 +7,29 @@ function devolverCasilla(i, j) {
     return document.getElementById(i + "_" + j)
 }
 
+function devolverCasillaT(i, j) {
+    return document.getElementById((i - 1) + "_" + j)
+}
+function devolverCasillaB(i, j) {
+    return document.getElementById((i + 1) + "_" + j)
+}
+function devolverCasillaR(i, j) {
+    return document.getElementById(i + "_" + (j + 1))
+}
+function devolverCasillaL(i, j) {
+    return document.getElementById(i + "_" + (j + 1))
+}
+
+
 function casillaOnClic(i, j) {
     console.log("Hizo Click en", i, j);
 
     const casilla = devolverCasilla(i, j);
+
+    const casillaT = devolverCasilla(i, j);
+    const casillaB = devolverCasilla(i, j);
+    const casillaR = devolverCasilla(i, j);
+    const casillaL = devolverCasilla(i, j);
 
     let idCasilla = i + "_" + j;
 
